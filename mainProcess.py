e6a5b9fbe69a2b60578ab918b989d9dbfc4abad4
derg0004 <derg0004@derg0004s-MacBook-Pro.local>
2018-04-28 16:41:06 -0500

    updating to latest version

--- mainProcess.py
+++ mainProcess.py
@@ -3,6 +3,8 @@ import timeit
 import math
 import tsp_util
 import clusters
+import matplotlib
+import random
 
 
 #MAIN-------------------------------
@@ -23,18 +25,31 @@ thirdList = []
 fourthCluster = []
 fourthList = []
 
-firstCluster,secondCluster,thirdCluster,fourthCluster = findClusters(list)
-firstCluster,secondCluster,thirdCluster,fourthCluster = FillClusters(firstCluster,secondCluster,thirdCluster,fourthCluster,list)
+firstCluster,secondCluster,thirdCluster,fourthCluster = clusters.findClusters(list)
+firstCluster,secondCluster,thirdCluster,fourthCluster = clusters.FillClusters(firstCluster,secondCluster,thirdCluster,fourthCluster,list)
+#Copy Lists so we still have them
+
+
+firstList.extend(firstCluster)
+secondList.extend(secondCluster)
+thirdList.extend(thirdCluster)
+fourthList.extend(fourthCluster)
 
 #find optimal path in each clusters
 sumList = []
+sum = 0
+print("\n")
+print(firstList)
+firstList, sum = tsp_util.nearestNeighbor(firstList)
+sumList.append(sum)
+secondList, sum = tsp_util.nearestNeighbor(secondList)
+sumList.append(sum)
+thirdList, sum = tsp_util.nearestNeighbor(thirdList)
+sumList.append(sum)
+fourthList, sum = tsp_util.nearestNeighbor(fourthList)
+sumList.append(sum)
 
-firstList, sumList[0] = tsp_util.nearestNeighbor(firstCluster)
-secondList, sumList[1] = tsp_util.nearestNeighbor(secondCluster)
-thirdList, sumList[2] = tsp_util.nearestNeighbor(thirdCluster)
-fourthList, sumList[3] = tsp_util.nearestNeighbor(fourthCluster)
 
-#Connect the Clusters
 
 #first sort the cluster, then get 2 max from each, shove into aggregate list
 cities = []
@@ -60,4 +75,9 @@ cities.append(fourthCluster.pop(0))
 cities.append(fourthCluster.pop(0))
 
 
-firstList = tsp_util.combinedList(firstList,secondList,thirdList,fourthList,cities)
+firstList = tsp_util.combineClusters(firstList,secondList,thirdList,fourthList,cities)
+
+
+print("\n route:", firstList)
+
+tsp_util.graphplot(firstList)
--- tsp_util.py
+++ tsp_util.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import math
+import random
 import matplotlib.pyplot as plt
 
 def closestpoint(point, route):
@@ -35,7 +36,11 @@ def findpath(point, list):
     path.append(closest)
     sum += dist
     return path, sum
+def sumDistance(list):
+    sum = 0
 
+
+    return sum
 def processFile(filename):
 
 
@@ -47,29 +52,73 @@ def processFile(filename):
         list.append([float(x), float(y)]) # int tuple list
 
 
-    file.close()
+    #file.close()
 
     return list
 
 def nearestNeighbor(list):
     random_number = random.randint(0, len(list)+1)
+    print(random_number)
 
-    point = list.pop(random_number) # pop out the first item in the list
+    point = list.pop(random_number) # pop out random item from list
 
-    path, sum = tsp_util.findpath(point, list)
+    path, sum = findpath(point, list)
     return path, sum
 
 def greedy(list):
     point = list.pop(0) # pop out the first item in the list
 
-    path, sum = tsp_util.findpath(point, list)
+    path, sum = findpath(point, list)
     return path, sum
 
 def combineClusters(firstList,secondList,thirdList,fourthList, cities):
-    combinedList = []
+
+
+    #cities [0]amax[1]amax [2]amin[3]amin
+
+    #cities [4]bmax [5]bmax [6]bmin [7min]
+
+    #cities [8]cmax [9]cmax [10]cmin [11cmin]
+
+    #cities [12]dmax [13]dmax [14]dmin [15]dmin
+
+    #connect amax to bmin
+
+    firstList.remove(cities[0])
+    firstList.remove(cities[1])
+    secondList.remove(cities[4])
+    secondList.remove(cities[5])
+    secondList.remove(cities[6])
+    secondList.remove(cities[7])
+    thirdList.remove(cities[8])
+    thirdList.remove(cities[9])
+    thirdList.remove(cities[10])
+    thirdList.remove(cities[11])
+    fourthList.remove(cities[14])
+    fourthList.remove(cities[15])
+
+
+    firstList.append(cities[0])
+    firstList.append(cities[6])
+    firstList.append(cities[1])
+    firstList.append(cities[7])
+
+    firstList.extend(secondList)
+    #ok up to this point
+    firstList.append(cities[4])
+    firstList.append(cities[10])
+    firstList.append(cities[5])
+    firstList.append(cities[11])
+
+    firstList.extend(thirdList)
+
+    firstList.append(cities[8])
+    firstList.append(cities[14])
+    firstList.append(cities[9])
+    firstList.append(cities[15])
+
+    firstList.extend(fourthList)
 
 
 
-
-
-    return combinedList
+    return firstList
